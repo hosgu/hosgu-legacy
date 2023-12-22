86792c4ef4ffd583c902684bfb03f1f1dbe7f370
FoggDev <51180821+FoggDev@users.noreply.github.com>
2023-12-22 14:27:57 -0600

    Update index.tsx

--- src/app/components/Hero/index.tsx
+++ src/app/components/Hero/index.tsx
@@ -5,7 +5,6 @@ import { Input } from '@architecturex/components.input'
 import { Button } from '@architecturex/components.button'
 import { Translations } from '~app/i18n'
 import Select from '~components/Select'
-import PhoneNumberInput from '~components/PhoneNumberInput'
 
 type Props = {
   t: Translations
@@ -26,7 +25,7 @@ const Hero: FC<Props> = ({ t }) => {
         <input
           type="text"
           placeholder="Full name"
-          className="border p-2 border-gray-300 bg-white rounded text-black dark:border-gray-600 dark:bg-gray-700 dark:text-white"
+          className="w-full border p-2 border-gray-300 bg-white rounded text-black dark:border-gray-600 dark:bg-gray-700 dark:text-white"
         />
       </div>
 
@@ -40,7 +39,7 @@ const Hero: FC<Props> = ({ t }) => {
         <input
           type="text"
           placeholder="Business name"
-          className="border p-2 border-gray-300 bg-white rounded text-black dark:border-gray-600 dark:bg-gray-700 dark:text-white"
+          className="w-full border p-2 border-gray-300 bg-white rounded text-black dark:border-gray-600 dark:bg-gray-700 dark:text-white"
         />
       </div>
     </div>,
@@ -55,7 +54,7 @@ const Hero: FC<Props> = ({ t }) => {
         <input
           type="text"
           placeholder="Business email"
-          className="border p-2 border-gray-300 bg-white rounded text-black dark:border-gray-600 dark:bg-gray-700 dark:text-white"
+          className="w-full border p-2 border-gray-300 bg-white rounded text-black dark:border-gray-600 dark:bg-gray-700 dark:text-white"
         />
       </div>
       <div className="p-4 text-left">
@@ -68,7 +67,7 @@ const Hero: FC<Props> = ({ t }) => {
         <input
           type="text"
           placeholder="+52 312 123 4567"
-          className="border p-2 border-gray-300 bg-white rounded text-black dark:border-gray-600 dark:bg-gray-700 dark:text-white"
+          className="w-full border p-2 border-gray-300 bg-white rounded text-black dark:border-gray-600 dark:bg-gray-700 dark:text-white"
         />
       </div>
     </div>,
@@ -83,7 +82,7 @@ const Hero: FC<Props> = ({ t }) => {
         <input
           type="text"
           placeholder="https://"
-          className="border p-2 border-gray-300 bg-white rounded text-black dark:border-gray-600 dark:bg-gray-700 dark:text-white"
+          className="w-full border p-2 border-gray-300 bg-white rounded text-black dark:border-gray-600 dark:bg-gray-700 dark:text-white"
         />
       </div>
 
@@ -92,12 +91,12 @@ const Hero: FC<Props> = ({ t }) => {
           className="block text-gray-700 text-sm font-bold mb-2 text-left dark:text-gray-300"
           htmlFor="username"
         >
-          City
+          Country
         </label>
         <input
           type="text"
-          placeholder="City"
-          className="border p-2 border-gray-300 bg-white rounded text-black dark:border-gray-600 dark:bg-gray-700 dark:text-white"
+          placeholder="Mexico"
+          className="w-full border p-2 border-gray-300 bg-white rounded text-black dark:border-gray-600 dark:bg-gray-700 dark:text-white"
         />
       </div>
     </div>
@@ -105,13 +104,13 @@ const Hero: FC<Props> = ({ t }) => {
 
   return (
     <div
-      className="max-w-xLarge m-auto relative h-screen px-8 text-center bg-cover bg-center bg-no-repeat bg-white"
+      className="max-w-xLarge m-auto relative h-[900px] lg:h-screen px-8 text-center bg-cover bg-center bg-no-repeat bg-white"
       style={{ backgroundImage: `url(${image})` }}
     >
       <div className="flex justify-between items-center flex-col lg:flex-row pt-5 lg:pt-60">
         <div className="relative z-10 align-center">
           <h1>Boost Your Business Revenue by +30% in just 6 months</h1>
-          <p className="pr-6 pb-2">
+          <p className="pr-6 pb-4">
             Elevate your bookings by +30% in just six months. Our intelligent platform streamlines
             your workflow, slashing operational expenses and freeing up to 80% of your
             time—empowering you to focus on other areas of your business. Transform your booking
@@ -119,7 +118,7 @@ const Hero: FC<Props> = ({ t }) => {
           </p>
         </div>
 
-        <div className="border-gray-300 shadow-xl rounded bg-white dark:bg-gray-800 w-[480px] lg:w-[800px] pt-6">
+        <div className="border-gray-300 shadow-xl rounded bg-white dark:bg-gray-800 w-[400px] md:w-[480px] lg:w-[800px] pt-6">
           {form}
 
           <div className="flex justify-center mb-6 mt-6">
