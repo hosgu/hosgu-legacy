5dfccdf0dfa5907cc9ca6bd071500f277991ac78
FoggDev <51180821+FoggDev@users.noreply.github.com>
2024-01-30 17:38:09 -0800

    Fixing Delete Guests Table

--- src/app/[locale]/dashboard/guests/GuestTable.tsx
+++ src/app/[locale]/dashboard/guests/GuestTable.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { FC, useState } from 'react'
+import { FC, useState, useEffect, useCallback } from 'react'
 import Table from '~/components/Table'
 import Button from '~/components/Button'
 import core from '@architecturex/utils.core'
@@ -9,33 +9,59 @@ type Props = {
   data: any[]
 }
 
-const GuestTable: FC<Props> = ({ data }) => {
-  const initialRows = data.map(({ id, fullName, email, phone, website, gender, birthday }: any) => [
-    fullName,
-    email,
-    phone,
-    website,
-    gender,
-    birthday,
-    <a key={`delete-${fullName}`} href="#" onClick={() => handleDelete(id)}>
-      X
-    </a>
-  ])
+const GuestTable: FC<Props> = ({ data: rawData = [] }) => {
+  // Initial states
+  const [data, setData] = useState(rawData)
 
+  // Methods
+  const handleDelete = useCallback(
+    async (id: string) => {
+      const formData = core.formData.set(new FormData(), {
+        id
+      })
+
+      const response = await deleteGuestServerAction(formData)
+
+      if (response.ok) {
+        const filteredData = data.filter((guest: any) => guest.id !== id)
+
+        setData(filteredData)
+      }
+    },
+    [data]
+  )
+
+  const getRows = useCallback(
+    () =>
+      data?.map(({ id, fullName, email, phone, website, gender, birthday }: any) => [
+        fullName,
+        email,
+        phone,
+        website,
+        gender,
+        birthday,
+        <a key={`delete-${fullName}`} href="#" onClick={() => handleDelete(id)}>
+          X
+        </a>
+      ]) || [],
+    [data, handleDelete]
+  )
+
+  // States
+  const initialRows = getRows()
   const [rows, setRows] = useState(initialRows)
 
-  const handleDelete = (id: string) => {
-    const formData = core.formData.set(new FormData(), {
-      id
-    })
+  useEffect(() => {
+    const newRows = getRows()
 
-    deleteGuestServerAction(formData)
-  }
+    setRows(newRows)
+  }, [data, getRows])
 
   const headers = ['Full name', 'Email', 'Phone', 'Links', 'Gender', 'Birthday', 'Actions']
 
   return (
     <Table
+      key={`table-${rows.length}`}
       label="Guests"
       createButton={
         <Button color="info" size="small" href="guests/create">
