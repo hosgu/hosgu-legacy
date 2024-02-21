6ccd2756711c9b9627896a3f1522d3ee73dfa958
Daniel Cortes <danieldevcode@gmail.com>
2024-02-20 16:46:57 -0800

    add: ReservationsTable component

---
+++ src/app/dashboard/components/Guests/ReservationsTable.tsx
@@ -0,0 +1,42 @@
+'use client'
+import Table from '~/components/Table'
+import { ReservationFields } from '~/server/db/schemas/reservation'
+
+function ReservationsTable({ reservations }: Props) {
+  const headers = [
+    'Photo',
+    'Name',
+    'Paid',
+    'Pending',
+    'Total',
+    'Discount',
+    'Booking date',
+    'Check out date'
+  ]
+  const rows = reservations.map((reservation) => {
+    return [
+      <img
+        key={'property.photo'}
+        src={
+          'https://images.pexels.com/photos/803975/pexels-photo-803975.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+        }
+        alt={'property.name'}
+        className="rounded-lg max-w-[196px]"
+      />,
+      'property.name',
+      reservation.paidAmount,
+      reservation.pendingAmount,
+      reservation.totalAmount,
+      reservation.discount,
+      reservation.startDate,
+      reservation.endDate
+    ]
+  })
+  return <Table headers={headers} rows={rows} label="Reservation history" />
+}
+
+type Props = {
+  reservations: ReservationFields[]
+}
+
+export default ReservationsTable
