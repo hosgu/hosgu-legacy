15e2649c92df7127ff519baaf42ef399e2223346
Daniel Cortes <danieldevcode@gmail.com>
2024-02-20 16:49:07 -0800

    add: reservation custom route

--- src/server/api/v1/reservation/index.ts
+++ src/server/api/v1/reservation/index.ts
@@ -1,6 +1,25 @@
+import { RequestHandler } from 'express'
 import { createCRUDRoutes } from '../../routerGenerator'
 import CRUD from './reservation'
+import { db } from '../../../db/index'
 
-const router = createCRUDRoutes(CRUD)
+var customCRUD = new CRUD(db)
+
+var customRoutes: Record<string, { method: string; handler: RequestHandler }> = {
+  '/guest/:id': {
+    method: 'GET',
+    handler: async (req, res) => {
+      try {
+        var { id } = req.params
+        var response = await customCRUD.getByGuestId(id)
+        res.json({ ok: true, data: response })
+      } catch (error) {
+        res.status(500).json({ ok: false, error: error })
+      }
+    }
+  }
+}
+
+const router = createCRUDRoutes(CRUD, customRoutes)
 
 export default router
--- src/server/api/v1/reservation/reservation.ts
+++ src/server/api/v1/reservation/reservation.ts
@@ -1,10 +1,32 @@
 import CRUDHandler from '../../CRUD'
 import { Reservation, reservation } from '../../../db/schemas/reservation'
 import { DB } from '../../../db'
+import { eq } from 'drizzle-orm'
+import { DataResponse, ItemData } from '~/server/api/types'
+import { GuestFields } from '~/server/db/schemas/guest'
 
 class CRUD extends CRUDHandler<Reservation> {
   constructor(db: DB) {
     super(db, reservation)
+  }
+
+  async getByGuestId(id: GuestFields['id']): Promise<DataResponse<ItemData>> {
+    const data = await this.db
+      .select()
+      .from(this.table)
+      .where(eq(reservation.guestId, id))
+
+    if (data.length === 0) {
+      throw {
+        type: 'NOT_FOUND_ERROR',
+        code: 'NO_ITEM_FOUND',
+        message: 'noItemFound'
+      }
+    }
+
+    return {
+      items: data
+    }
   }
 }
 
