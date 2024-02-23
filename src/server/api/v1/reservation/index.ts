e41dca1254f1162ffea4333bab4bc76b66431fce
Daniel Cortes <danieldevcode@gmail.com>
2024-02-23 15:16:15 -0800

    ref: var to const

--- src/server/api/v1/reservation/index.ts
+++ src/server/api/v1/reservation/index.ts
@@ -3,15 +3,15 @@ import { createCRUDRoutes } from '../../routerGenerator'
 import CRUD from './reservation'
 import { db } from '../../../db/index'
 
-var customCRUD = new CRUD(db)
+const customCRUD = new CRUD(db)
 
-var customRoutes: Record<string, { method: string; handler: RequestHandler }> = {
+const customRoutes: Record<string, { method: string; handler: RequestHandler }> = {
   '/guest/:id': {
     method: 'GET',
     handler: async (req, res) => {
       try {
-        var { id } = req.params
-        var response = await customCRUD.getByGuestId(id)
+        const { id } = req.params
+        const response = await customCRUD.getByGuestId(id)
         res.json({ ok: true, data: response })
       } catch (error) {
         res.status(500).json({ ok: false, error: error })
