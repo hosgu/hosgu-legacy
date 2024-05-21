c6405f96885ad3e47cf03ea2db86e6ec638b6968
locallyhome <106122914+locallyhome@users.noreply.github.com>
2024-05-21 16:29:14 -0700

    Issue #97 - Import react number format

--- src/app/profile/setup/components/Step5.tsx
+++ src/app/profile/setup/components/Step5.tsx
@@ -1,5 +1,8 @@
 'use client'
 import React, { FC, useEffect } from 'react'
+import { NumericFormat } from 'react-number-format'
+import cx from '@architecturex/utils.cx'
+
 import Link from '~/app/shared/components/Link'
 import Checkbox from '~/components/Checkbox'
 import Input from '~/components/Input'
@@ -33,29 +36,63 @@ const Step: FC<Props> = ({ locale, setStep, setValues, values, setEnableNext }) 
   return (
     <div className="flex flex-col justify-between space items-center text-center w-full max-w-[500px] ">
       <div className="flex flex-row">
-        <Input
-          value={priceNights}
-          label="Price per night"
-          className="rounded-lg w-lg"
-          type="number"
-          onChange={(e: any) => setValues({ ...values, priceNights: e.target.value })}
-        />{' '}
-        <Input
-          value={cleaningFee}
-          label="cleaning fee"
-          type="number"
-          className="rounded-lg w-lg"
-          onChange={(e: any) => setValues({ ...values, cleaningFee: e.target.value })}
-        />
+        <div className={cx.join('p-4 text-left')}>
+          <label
+            htmlFor=""
+            className="block text-gray-700 text-sm font-bold mb-2 text-left dark:text-gray-300 "
+          >
+            price per night
+          </label>
+          <NumericFormat
+            prefix={'$'}
+            className={cx.join(
+              'w-full border p-2 border-gray-300 bg-white rounded text-black dark:border-gray-600 dark:bg-gray-700 dark:text-white rounded-lg w-lg'
+            )}
+            value={priceNights}
+            allowLeadingZeros
+            thousandSeparator=","
+            onChange={(e: any) => setValues({ ...values, priceNights: e.target.value })}
+          />
+        </div>
+
+        <div className={cx.join('p-4 text-left')}>
+          <label
+            htmlFor=""
+            className="block text-gray-700 text-sm font-bold mb-2 text-left dark:text-gray-300"
+          >
+            cleaning fee
+          </label>
+          <NumericFormat
+            prefix={'$'}
+            className={cx.join(
+              'w-full border p-2 border-gray-300 bg-white rounded text-black dark:border-gray-600 dark:bg-gray-700 dark:text-white rounded-lg w-lg'
+            )}
+            value={cleaningFee}
+            allowLeadingZeros
+            thousandSeparator=","
+            onChange={(e: any) => setValues({ ...values, cleaningFee: e.target.value })}
+          />
+        </div>
       </div>
       <div className="flex flex-row">
-        <Input
-          value={extraPersonPrice}
-          label="price per extra person"
-          className="rounded-lg w-lg"
-          type="number"
-          onChange={(e: any) => setValues({ ...values, extraPersonPrice: e.target.value })}
-        />
+        <div className={cx.join('p-4 text-left')}>
+          <label
+            htmlFor=""
+            className="block text-gray-700 text-sm font-bold mb-2 text-left dark:text-gray-300"
+          >
+            price per extra person
+          </label>
+          <NumericFormat
+            prefix={'$'}
+            className={cx.join(
+              'w-full border p-2 border-gray-300 bg-white rounded text-black dark:border-gray-600 dark:bg-gray-700 dark:text-white rounded-lg w-lg'
+            )}
+            value={extraPersonPrice}
+            allowLeadingZeros
+            thousandSeparator=","
+            onChange={(e: any) => setValues({ ...values, extraPersonPrice: e.target.value })}
+          />
+        </div>
         <div className="flex flex-row w-9/12">
           <Input
             value={checkIn}
