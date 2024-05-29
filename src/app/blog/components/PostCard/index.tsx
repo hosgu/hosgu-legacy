af3fbaa014e61f7534f3a7dd08b0b6d848231955
Montserrat Aguilar <aguilarvalle.montse@gmail.com>
2024-05-28 20:44:49 -0600

    Add: create PostCard component

---
+++ src/app/blog/components/PostCard/index.tsx
@@ -0,0 +1,30 @@
+'use client'
+
+const PostCard = ({ title, summary, category, author, postDate }: Props) => {
+  return (
+    <div className="items-stretch">
+      <p className="pt-2 text-[12px] leading-4 text-neutral-600">{category}</p>
+      <h2 className="p-0 text-[24px] font-bold tracking-tight leading-8">{title}</h2>
+      {/* <p className="pb-2">{summary}</p> */}
+
+      <div className="flex flex-row-reverse justify-end items-center gap-3 pt-2">
+        <div>
+          <p className="m-0 text-[14px] text-neutral-600">{postDate}</p>
+        </div>
+        <div className="h-[25px] w-[25px]">
+          <img className="w-6 h-6 rounded-full" src={author.image} alt={author.name} />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+interface Props {
+  title: string
+  postDate: string
+  summary: string
+  category: string
+  author: { link: string; role: string; name: string; image: string }
+}
+
+export default PostCard
