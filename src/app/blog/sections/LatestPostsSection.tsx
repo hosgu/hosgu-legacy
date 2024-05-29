869098f7bd3b0bd7dac6e5120e0cc0637cccb83a
Montserrat Aguilar <aguilarvalle.montse@gmail.com>
2024-05-29 14:54:41 -0600

    Add: 'Latest' title and 'Load More' button

--- src/app/blog/sections/LatestPostsSection.tsx
+++ src/app/blog/sections/LatestPostsSection.tsx
@@ -29,19 +29,34 @@ const LatestSection = ({ category }: Props) => {
 
   return (
     <>
-      <div className="bg-gray-100 grid gap-y-10 md:gap-y-16 md:gap-x-16 md:grid-cols-2 p-4 md:px-28">
-        {posts.map((item: any) => {
-          return (
-            <PostCard
-              key={item.id}
-              title={item.title}
-              postDate={item.postDate}
-              summary={item.summary}
-              category={item.category}
-              author={item.author}
-            />
-          )
-        })}
+      <div className="flex flex-col justify-start items-stretch gap-10 md:py-16 bg-gray-100 p-4 md:px-28 ">
+        <div>
+          <h2 className="text-[40px] text-bold tracking-tight leading-10">Latest</h2>
+        </div>
+        <div className="grid gap-y-10 md:gap-y-16 md:gap-x-16 md:grid-cols-2">
+          {posts.map((item: any) => {
+            return (
+              <PostCard
+                key={item.id}
+                title={item.title}
+                postDate={item.postDate}
+                summary={item.summary}
+                category={item.category}
+                author={item.author}
+              />
+            )
+          })}
+        </div>
+        <div className="flex justify-center pt-6">
+          <button
+            type="button"
+            className="flex items-center rounded-md shadow-md border font-normal justify-center leading-5 text-center border-zinc-800 px-32 py-1 bg-gray-200 hover:bg-gray-300"
+          >
+            <span className="text-base items-center font-medium tracking-tight leading-10">
+              Load More
+            </span>
+          </button>
+        </div>
       </div>
     </>
   )
