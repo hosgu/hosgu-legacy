20903d88851a27559c3bf882c170535b0a76d666
Daniel Cortes <danieldevcode@gmail.com>
2024-06-04 15:45:53 -0700

    ref: moved category page

--- src/app/blog/[category]/page.tsx
+++
@@ -1,9 +0,0 @@
-'use client'
-import LatestSection from '../sections/LatestPostsSection'
-
-const CategoryPage = ({ params }) => {
-  const { category } = params
-  return <LatestSection category={category} />
-}
-
-export default CategoryPage
