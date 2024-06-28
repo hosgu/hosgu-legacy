9ad844966e636620f1427871a56e9b8bb899bc83
FoggDev <51180821+FoggDev@users.noreply.github.com>
2024-06-27 17:21:42 -0700

    Adding Reports

--- src/app/control/[businessId]/page.tsx
+++ src/app/control/[businessId]/page.tsx
@@ -2,6 +2,11 @@ import { FC } from 'react'
 
 const Page: FC = async ({ params }: any) => {
   console.log('PARAMS===>', params)
+
+  // Fetch to get Business Data
+
+  // If business data is not found, return 404
+
   return (
     <div className="min-h-screen flex bg-gray-100 dark:bg-gray-900">
       <div className="p-2 dark:text-white w-full">
