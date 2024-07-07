fab7862cf269b9badf51d61854b3b510e03e4a8d
Saul Rosales B <saul.rosalesb@gmail.com>
2024-07-07 17:36:48 -0400

    Removed unused server action

--- src/app/shared/actions/profileSetUp.ts
+++
@@ -1,6 +0,0 @@
-import setUpProfileService from '../services/profileSetup'
-
-export const setUpProfile = async (propertyData: any) => {
-  const response = await setUpProfileService.setUpProfile(propertyData)
-  return response
-}
