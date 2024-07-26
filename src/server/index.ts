43bb1f90841a8a68e8a6920b76bf4ccc3c9ec790
FoggDev <foggdev@gmail.com>
2024-07-26 16:16:09 -0700

    Adding SSL and domain validator

--- src/server/index.ts
+++ src/server/index.ts
@@ -4,7 +4,7 @@ import cors from 'cors'
 import express, { Application, NextFunction, Request, Response } from 'express'
 import nextJS from 'next'
 import path from 'path'
-import { createServer } from 'http'
+import nextBuild from 'next/dist/build'
 
 // APIs
 import agentApiV1 from './api/v1/agent'
@@ -28,6 +28,7 @@ import userApiV1 from './api/v1/user'
 
 const basePort = 3000
 const dev = process.env.NODE_ENV !== 'production'
+
 const nextApp = nextJS({ dev })
 const handle = nextApp.getRequestHandler()
 
@@ -36,87 +37,95 @@ const corsOptions = {
   credentials: true
 }
 
-function findAvailablePort(startingPort: number, callback: (port: number) => void) {
-  const server = createServer()
-  server.unref()
-  server.on('error', () => {
-    findAvailablePort(startingPort + 1, callback)
+// Express application
+const app: Application = express()
+
+const start = async (): Promise<void> => {
+  app.use(cors(corsOptions))
+
+  // Cookies
+  app.use(cookieParser())
+
+  app.use(cors(corsOptions))
+  app.use(bodyParser.json())
+
+  // Sites static directories
+  app.use(express.static(path.join(__dirname, '../../public')))
+
+  app.use((req: any, res: Response, next: NextFunction) => {
+    req.cwd = process.cwd()
+    next()
   })
-  server.listen(startingPort, () => {
-    server.close(() => {
-      callback(startingPort)
-    })
+
+  // API
+  app.use('/api/v1/agent', agentApiV1)
+  app.use('/api/v1/asr', asrApiV1)
+  app.use('/api/v1/asset', assetApiV1)
+  app.use('/api/v1/business', businessApiV1)
+  app.use('/api/v1/cancellation', cancellationApiV1)
+  app.use('/api/v1/commission', commissionApiV1)
+  app.use('/api/v1/employee', employeeApiV1)
+  app.use('/api/v1/fee', feeApiV1)
+  app.use('/api/v1/guest', guestApiV1)
+  app.use('/api/v1/photo', photoApiV1)
+  app.use('/api/v1/property', propertyApiV1)
+  app.use('/api/v1/reservation', reservationApiV1)
+  app.use('/api/v1/room', roomApiV1)
+  app.use('/api/v1/settings', settingsApiV1)
+  app.use('/api/v1/tier', tierApiV1)
+  app.use('/api/v1/unit', unitApiV1)
+  app.use('/api/v1/uploader', uploaderApiV1)
+  app.use('/api/v1/user', userApiV1)
+
+  // Logout
+  app.get('/logout', (req: Request, res: Response) => {
+    const redirect: any = req.query.redirectTo || '/'
+    res.clearCookie('at')
+    res.redirect(redirect)
   })
-}
 
-// Running Next App
-nextApp.prepare().then(() => {
-  findAvailablePort(basePort, (port) => {
-    // Express application
-    const app: Application = express()
+  // Setting userLanguage cookie
+  app.use((req: Request, res: Response, next: NextFunction) => {
+    const languages = (req.headers['accept-language'] || '').split(';')[0]
+    const userLanguage = languages.split(',')[0] || ''
+    let locale = 'en-us'
 
-    // Cookies
-    app.use(cookieParser())
+    if (userLanguage.includes('es')) {
+      locale = 'es-mx'
+    }
 
-    app.use(cors(corsOptions))
-    app.use(bodyParser.json())
+    res.cookie('locale', locale)
 
-    // Sites static directories
-    app.use(express.static(path.join(__dirname, '../public')))
+    next()
+  })
 
-    app.use((req: any, res: Response, next: NextFunction) => {
-      req.cwd = process.cwd()
-      next()
+  // Next.js build
+  if (process.env.NEXT_BUILD) {
+    app.listen(basePort, async () => {
+      // Traffic handling
+      app.all('*', (req: Request, res: Response) => handle(req, res))
+
+      console.log(`Next.js is now building...`)
+      // @ts-expect-error
+      await nextBuild(path.join(__dirname, '..'))
+      process.exit()
     })
 
-    // API
-    app.use('/api/v1/agent', agentApiV1)
-    app.use('/api/v1/asr', asrApiV1)
-    app.use('/api/v1/asset', assetApiV1)
-    app.use('/api/v1/business', businessApiV1)
-    app.use('/api/v1/cancellation', cancellationApiV1)
-    app.use('/api/v1/commission', commissionApiV1)
-    app.use('/api/v1/employee', employeeApiV1)
-    app.use('/api/v1/fee', feeApiV1)
-    app.use('/api/v1/guest', guestApiV1)
-    app.use('/api/v1/photo', photoApiV1)
-    app.use('/api/v1/property', propertyApiV1)
-    app.use('/api/v1/reservation', reservationApiV1)
-    app.use('/api/v1/room', roomApiV1)
-    app.use('/api/v1/settings', settingsApiV1)
-    app.use('/api/v1/tier', tierApiV1)
-    app.use('/api/v1/unit', unitApiV1)
-    app.use('/api/v1/uploader', uploaderApiV1)
-    app.use('/api/v1/user', userApiV1)
+    return
+  }
 
-    // Logout
-    app.get('/logout', (req: Request, res: Response) => {
-      const redirect: any = req.query.redirectTo || '/'
-      res.clearCookie('at')
-      res.redirect(redirect)
-    })
-
-    // Setting userLanguage cookie
-    app.use((req: Request, res: Response, next: NextFunction) => {
-      const languages = (req.headers['accept-language'] || '').split(';')[0]
-      const userLanguage = languages.split(',')[0] || ''
-      let locale = 'en-us'
-
-      if (userLanguage.includes('es')) {
-        locale = 'es-mx'
-      }
-
-      res.cookie('locale', locale)
-
-      next()
-    })
+  // Running Next App
+  nextApp.prepare().then(() => {
+    console.log('Next.js started')
 
     // Traffic handling
     app.all('*', (req: Request, res: Response) => handle(req, res))
 
     // Listening...
-    app.listen(port, () => {
-      console.log(`Server is running on port ${port}`)
+    app.listen(basePort, () => {
+      console.log(`Server is running on port ${basePort}`)
     })
   })
-})
+}
+
+start()
