c91a8d247d41a749e9f5d13cf6c0e2ad00bedebc
BrooksIvory91 <48321263+BrooksIvory91@users.noreply.github.com>
2024-09-07 12:23:11 -0700

    Added styling to report blocks in darkmode (#171)

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,9 +3,9 @@ import Home from '../control/components/Home'
 
 const DashboardPage: FC = async () => {
   return (
-    <div className="min-h-screen flex bg-gray-100 dark:bg-gray-900">
+    <div className="min-h-screen flex bg-gray-100 dark:bg-[black]">
       <div className="p-2 dark:text-white w-full">
-        <h1 className="text-2xl font-bold mb-4 text-center dark:text-white">Dashboard</h1>
+        <h1 className="text-2xl font-bold mb-4 text-center dark:text-slate-300">Dashboard</h1>
         <Home />
       </div>
     </div>
