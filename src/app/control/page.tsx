284c7e6432fe31e73fe0d937cf3e7a7eed16166a
Saul Rosales Bautista <saulrb@users.noreply.github.com>
2024-10-31 20:19:13 -0400

    Fixed Hash calculation to refresh Guest table

--- src/app/control/page.tsx
+++ src/app/control/page.tsx
@@ -1,14 +1,6 @@
 import { FC } from 'react'
-import { cookies } from 'next/headers'
-
-import * as UserActions from '~/app/core/actions/user'
 
 const Page: FC = async ({ params }: any) => {
-  const cookieStore = cookies()
-  const connectedUser = await UserActions.getConnectedUser(cookieStore.get('at')?.value || '')
-
-  console.log('CONNECTED USER===>', connectedUser)
-
   return (
     <div className="min-h-screen flex bg-gray-100 dark:bg-gray-900">
       <div className="p-2 dark:text-white w-full">
